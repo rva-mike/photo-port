36f86efca9f3334e125b3cd54598365027d9c141
rva-mike <mike.levy.rva@gmail.com>
2022-10-07 16:52:03 -0400

    ran tests

--- src/components/About/__tests__/index.test.js
+++ src/components/About/__tests__/index.test.js
@@ -1,23 +1,19 @@
+// __tests__/About.test.js
 import React from 'react';
 import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import About from '..';
 
-const { asFragment } = render(<About />);
-
-expect(asFragment()).toMatchSnapshot();
-
-
 afterEach(cleanup);
 
-describe('About component', () => {
-    // First Test
-    it('renders', () => {
-        render(<About />);
-    });
-
-    // Second Test
-    it('matches snapshot DOM node structure', () => {
-        // render About
-    });
+describe('About component renders', () => {
+  it('renders', () => {
+    render(<About />);
+  });
+  
+  it('matches snapshot DOM node structure', () => {
+    const { asFragment } = render(<About />);
+    
+    expect(asFragment()).toMatchSnapshot();
+  });
 })
